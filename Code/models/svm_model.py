2ac8daa2bcac1915c1347ddf0a5365fecd702571
arsoljon <jonathan.arsola01@utrgv.edu>
2020-11-18 14:20:34 -0600

    changed the x & y data into np.arrays() and added a visualization of the data. However, it doesn't make sense at the momemnt. It has X & Y data present on the graph but no way to differentiate between a w/d/l.

--- Code/models/svm_model.py
+++ Code/models/svm_model.py
@@ -6,6 +6,10 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.svm import SVC
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+from sklearn import datasets
 
 #Analyze Combine_data.csv first.
 #classes will be [w, d, l] = [0,0,0]
@@ -21,7 +25,12 @@ data = s.transform(data)
 #the next line helps organize the data easier
 scaled_df = pd.DataFrame(data, columns=df.columns)
 #data is 20rows x 13cols
-X = scaled_df.drop(['W', 'D', 'L'], axis=1)
-Y = scaled_df.loc[:,['W', 'D', 'L']]
+X = np.array(scaled_df.drop(['W', 'D', 'L'], axis=1))
+Y = np.array(scaled_df.loc[:,['W', 'D', 'L']])
 train_x, test_x, train_y, test_y = train_test_split(X, Y, test_size=0.3, random_state=0)
 
+#visulaize our data
+#will worry about this later.
+plt.plot(Y[0,0,], 'bo', markersize = 3)
+plt.plot(X, 'ro', markersize = 1)
+plt.show()
