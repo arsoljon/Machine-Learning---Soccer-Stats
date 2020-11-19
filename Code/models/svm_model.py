a511a2c3f2589ced35c730b8aaa1680089c456e2
arsoljon <jonathan.arsola01@utrgv.edu>
2020-11-19 04:11:07 -0600

    created the svm function for the betting odds data with the matches.

--- Code/models/svm_model.py
+++ Code/models/svm_model.py
@@ -3,6 +3,10 @@ import numpy as np
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
+import matplotlib
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+
 
 
 #Analyze Combine_data.csv first.
@@ -22,7 +26,7 @@ def combined_svm():
     scaled_df = pd.DataFrame(data, columns=df.columns)
     '''
     #data is 20rows x 13cols
-    X = np.array(df.drop(['W', 'D', 'L'], axis=1))
+    X = np.array(df.drop(['W', 'D', 'L'], axis=1), dtype=float)
     Y = np.array(df.loc[:,['W', 'D', 'L']])
     #change the greatest value of Y into 1 and make the rest 0
     #problem with this method is we lose a layer of accuracy
@@ -62,7 +66,6 @@ def combined_svm():
         print('Filename:', filename, file=f)
     print(acc)
 
-
     '''
     #visulaize our data
     #will worry about this later.
@@ -70,6 +73,7 @@ def combined_svm():
     plt.plot(X, 'ro', markersize = 1)
     plt.show()
     '''
+
 
 #make svm for matches without betting
 def matches_wobo_svm():
@@ -99,9 +103,60 @@ def matches_wobo_svm():
 
     train_x, test_x, train_y, test_y = train_test_split(X, Y, test_size=0.3, random_state=1)
 
-    model = SVC(kernel="linear")
+    model = SVC(kernel="rbf", degree=7)
     model.fit(train_x, train_y)
 
     pred_y = model.predict(test_x)
+
     acc = accuracy_score(test_y, pred_y)
     print(acc)
+
+def matches_bo_svm():
+    path = "C:/Users/Jonathan/PycharmProjects/Machine-Learning---Soccer-Stats/Data/matches-bo.csv"
+    df = pd.read_csv(path, sep=',', dtype=str)
+    df.drop(['Div', 'Date', 'Time', 'HomeTeam', 'AwayTeam', 'Referee'], axis=1, inplace=True)
+
+    # y is FTR, Full Time Result. Denoted with a H/D/A   for Home win/ Draw/ Away win
+    # Essentially, This will discover if teams win more often when they play as home or away.
+    # can try a  differeent y later. Maybe creating separate testing data with only team matches.
+    # can add referee id later to include referees
+    # change the values for HTR, Half time results.
+    df.loc[:, 'HTR'][df.loc[:, 'HTR'] == 'H'] = 1.0
+    df.loc[:, 'HTR'][df.loc[:, 'HTR'] == 'D'] = 0.0
+    df.loc[:, 'HTR'][df.loc[:, 'HTR'] == 'A'] = -1.0
+    X = np.array(df.drop(['FTR'], axis=1))
+    Y = np.array(df.loc[:, ['FTR']])
+    # Change string values to numbers for y. H/D/A
+    ytemp = []
+    for r in Y:
+        if (r == 'H'):
+            ytemp.append(1)
+        elif (r == 'D'):
+            ytemp.append(0)
+        elif (r == 'A'):
+            ytemp.append(-1)
+    Y = ytemp
+
+    # Normilize data
+    datax = X
+    datax = datax.astype(np.float)
+    s = MinMaxScaler()
+    s.fit(datax)
+    datax = s.transform(datax)
+    print(len(datax))
+    print(len(datax[:,:1]))
+
+
+
+    train_x, test_x, train_y, test_y = train_test_split(X, Y, test_size=0.3, random_state=1)
+
+    model = SVC(kernel="rbf", degree=1)
+    model.fit(train_x, train_y)
+
+    pred_y = model.predict(test_x)
+
+    acc = accuracy_score(test_y, pred_y)
+    print(acc)
+
+    #plt.scatter(test_x[:,:1], test_y, data=None )
+    #plt.show()
